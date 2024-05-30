0a809d60f69c60a4c4d9a887c7011293b810e5b7
Cody Clark <codyjamesclark@gmail.com>
2024-05-30 15:57:07 -0400

    made potteryWheel module and created 5 pieces of pottery in main

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,7 +5,9 @@ import { makePottery } from "./PotteryWheel.js"
 // Make 5 pieces of pottery at the wheel
 let mug = makePottery("Mug", 5, 3)
 let bowl = makePottery("Bowl", 10, 2)
-console.log(bowl)
+let plate = makePottery("Plate", 9, 1)
+let pitcher = makePottery("Pitcher", 25, 12)
+let vase = makePottery("Vase", 50, 25)
 
 
 // Fire each piece of pottery in the kiln
